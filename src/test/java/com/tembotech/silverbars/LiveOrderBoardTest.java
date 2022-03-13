f1da5a0937f1a11ef00b0fac320f5d9290afd7f1
Sameer Shah <sameer@zerocode.io>
2022-03-13 15:12:33 +0000

    Added new test

--- src/test/java/com/tembotech/silverbars/LiveOrderBoardTest.java
+++ src/test/java/com/tembotech/silverbars/LiveOrderBoardTest.java
@@ -150,6 +150,17 @@ public class LiveOrderBoardTest {
 
     }
 
+    public void test_order_summary_sell2() {
+
+        liveOrderBoard.register(new Order("Sally234",3.0, 500.00, OrderType.SELL));
+        liveOrderBoard.register(new Order("Pete123",2.5, 350.00, OrderType.SELL));
+        liveOrderBoard.register(new Order("Harry123",2.5, 450.00, OrderType.SELL));
+
+        assertTrue(liveOrderBoard.getOrderSummary().size() == 3);
+
+
+    }
+
 
 
 }
